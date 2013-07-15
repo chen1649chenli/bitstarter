8334f78bd42d881ae2cdef5336d166ca62d2484a
Li Chen <chen1649chenli@gmail.com>
2013-07-15 04:02:12 +0000

    update web.js

--- web.js
+++ web.js
@@ -2,11 +2,10 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var fs = require("fs");
-var buffer = fs.readFileSync("/home/ubuntu/bitstarter/index.html", "utf-8");
 
 app.get('/', function(request, response) {
-  response.send(buffer);
+var fs = require("fs");
+var buffer = fs.readFileSync("/home/ubuntu/bitstarter/index.html", "utf-8");
 });
 
 var port = process.env.PORT || 5000;
