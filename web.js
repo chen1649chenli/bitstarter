e380c441956aaa025fcd3e4454318449a9879e9b
Li Chen <chen1649chenli@gmail.com>
2013-07-15 03:43:52 +0000

    update web.js

--- web.js
+++ web.js
@@ -3,10 +3,14 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(test);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+var buffer = fs.readFileSync("/home/ubuntu/bitstarter/index.html");
+var test = buffer.toString("utf-8");
+
